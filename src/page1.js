8ddbab800369605219ed045004768f3dff55ea5a
Thibaut Matzke <matzket@frene04.iut-infobio.priv.univ-lille1.fr>
2018-03-22 08:45:51 +0100

    ajout des dossiers

---
+++ src/page1.js
@@ -0,0 +1,21 @@
+	$(document).ready(function(){
+	$("#pageEnregistrer").hide();	
+	$("#pageIdentifier").hide();
+	$("#confirmer").hide();
+	
+    $("#enregistrer").click(function(){
+		$("#page1").hide();
+		$("#pageEnregistrer").show();
+
+					
+	});
+	
+	$("#identifier").click(function(){
+		$("#page1").hide();
+		$("#pageIdentifier").show();			
+	});
+
+
+	});
+  
+    
