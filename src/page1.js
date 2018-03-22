eaee2c9c69809358e8c5a87b8526dd5693bc2ae1
Thibaut Matzke <matzket@frene04.iut-infobio.priv.univ-lille1.fr>
2018-03-22 09:10:37 +0100

    page

--- src/page1.js
+++ src/page1.js
@@ -5,15 +5,30 @@
 	
     $("#enregistrer").click(function(){
 		$("#page1").hide();
-		$("#pageEnregistrer").show();
-
-					
+		$("#pageEnregistrer").show();				
 	});
 	
 	$("#identifier").click(function(){
 		$("#page1").hide();
 		$("#pageIdentifier").show();			
 	});
+
+	$('#confimerE').click(function (event) {
+  	$.ajax({
+    url: "",
+    data: {
+        mail: 
+    },
+
+    type: "POST",
+    dataType : "json",
+    // Code to run if the request succeeds;
+    // the response is passed to the function
+    success: function( json ) {
+        $( "<h1>" ).text( json.title ).appendTo( "body" );
+        $( "<div class=\"content\">").html( json.html ).appendTo( "body" );
+    },
+
 
 
 	});
